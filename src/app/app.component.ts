34f888f2a79823c1cc5e57ff0862c7e52db84c5f
pm3f <67307376+pm3f@users.noreply.github.com>
2021-09-16 14:16:12 +1000

    added esri-loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+// import esri_map from '@arcgis/core/Map'
+// import esri_mapView from '@arcgis/core/views/MapView'
+import {loadModules} from 'esri-loader';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'StateMap';
+export class AppComponent {  title = 'StateMap';
+  // Get a container link for map place
+  @ViewChild('mapView', {static: true}) private readonly mapViewElement: ElementRef;
+  // main map view
+  private mapView;
+
+  constructor() {
+    // This function to load Dojo's require the classes listed in the array modules
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/MapImageLayer'])
+      .then(([Map, MapView, MapImageLayer]: [__esri.MapConstructor, __esri.MapViewConstructor,
+        __esri.MapImageLayerConstructor]) => {
+        // set default map properties
+        //
+        const mapProperties = {
+          basemap: 'gray'
+        };
+        // create map by default properties
+        const map = new Map(mapProperties);
+        // set default map view properties
+        // container - element in html-template for locate map
+        // zoom - default zoom parameter, value from 1 to 18
+        const mapViewProperties = {
+          container: this.mapViewElement.nativeElement,
+          zoom: 3,
+          map
+        };
+        // create map view by default properties
+        this.mapView = new MapView(mapViewProperties);
+
+        // // Set service properties
+        // // url - this address to MapServer from ArcGIS Enterprise
+        // // sublayers - this are the settings for the inner layers of the service.
+        // // id = 1 it tell us that will be displayed only one layer with the identifier
+        // const oilSandLayerProperties = {
+        //   url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/MapServer',
+        //   sublayers: [{id: 1}]
+        // };
+        // // Create map image layer by properties
+        // const oilSandsLayer = new MapImageLayer(oilSandLayerProperties);
+        // // Adding a layer into map
+        // map.add(oilSandsLayer);
+      });
+  }
+
 }
